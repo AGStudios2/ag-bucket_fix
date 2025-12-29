b775d42b4cbd78258e11ebf72fe6087cdb178a67
AG Studios <lildraco630@gmail.com>
2025-12-29 13:07:20 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AG Studios'
+description 'QBox routing bucket fix (Admin Only + Auto Fail-Safe)'
+version '1.1.0'
+
+server_script 'server.lua'
+client_script 'client.lua'
